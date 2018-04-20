3ec3c17465224c87acc7fac9ced5ea60febbc246
Artem Rugilov <artem.rugilov@digitaledtech.com>
2018-04-20 17:41:04 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React, {Component} from 'react';
 import Main from 'components/layout/Main';
 import cx from 'classnames';
 import {Route, HashRouter} from 'react-router-dom'
-import styles from './styles.module.sass';
-
-import './App.css';
+import './App.sass';
 
 class App extends Component {
   render() {
     return (
-      <div className={cx('App', styles.component)}>
+      <div className={cx('App')}>
         <HashRouter>
           <Route component={Main}/>
         </HashRouter>
